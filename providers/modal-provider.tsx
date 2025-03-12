9b9e354182429724f6477d0a7d56e01354011a92
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-12 15:52:39 +0100

    created modal-provider

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+}
