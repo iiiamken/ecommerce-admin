6dbe3ff65246c2ba22c8e1dab533be390e46e04b
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-18 23:40:54 +0100

    added isOpen and onOpen

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,21 @@
 "use client"
 import Modal from "@/components/ui/modal"
+import { useStoreModal } from "../../../hooks/use-store-modal"
+import { useEffect } from "react"
 
 const SetupPage = () => {
+  const { onOpen, isOpen } = useStoreModal()
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [onOpen, isOpen])
+
   return (
     <div className="p-4">
       <Modal title="test" description="test desc" onClose={() => {}} isOpen>
-        kids
+        Root Page
       </Modal>
     </div>
   )
