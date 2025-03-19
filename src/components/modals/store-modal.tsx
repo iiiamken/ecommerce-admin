f054fd500998827a96b6169f236b7674a0587c40
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-19 17:08:40 +0100

    added formMessage and cleaned imports

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,13 +1,20 @@
 "use client"
 
-import { useStoreModal } from "../../../hooks/use-store-modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
 import * as z from "zod"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
-import { Input } from "../ui/input"
-import { Button } from "../ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -43,6 +50,7 @@ export const StoreModal = () => {
                     <FormControl>
                       <Input placeholder="E-Commerce" {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
