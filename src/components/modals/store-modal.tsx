8ea4120b683f0bbd6764868be81475da26325cca
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-19 17:02:05 +0100

    added modal

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -2,9 +2,27 @@
 
 import { useStoreModal } from "../../../hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
 
+const formSchema = z.object({
+  name: z.string().min(1),
+})
 export const StoreModal = () => {
   const storemodal = useStoreModal()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { name: "" },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
   return (
     <Modal
       title="Create store"
@@ -12,7 +30,32 @@ export const StoreModal = () => {
       isOpen={storemodal.isOpen}
       onClose={storemodal.onClose}
     >
-      Future create store modal
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-Commerce" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={storemodal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   )
 }
