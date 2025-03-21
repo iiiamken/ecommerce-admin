2fefdc39a3c1aad4e1f1d738b0f4d4e58a8bb7f3
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-21 21:21:47 +0100

    added routes in main nav

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,8 +1,41 @@
 "use client"
 
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { useParams, usePathname } from "next/navigation"
+
 export function MainNav({
   className,
   ...props
 }: React.HtmlHTMLAttributes<HTMLElement>) {
-  return <p>Main navigaiton</p>
+  const pathName = usePathname()
+  const params = useParams()
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathName === `/${params.storeId}/settings`,
+    },
+  ]
+  return (
+    <p>
+      {routes.map((route) => {
+        return (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-primary",
+              route.active
+                ? "text-black dark:text-white"
+                : "text-muted-foreground"
+            )}
+          >
+            {route.label}
+          </Link>
+        )
+      })}
+    </p>
+  )
 }
