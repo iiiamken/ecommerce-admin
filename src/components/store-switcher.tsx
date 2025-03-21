ad594e9099c22487aae3745e73cc2f8170856f7b
iiiamken <kenny.t.dinh93@gmail.com>
2025-03-21 23:40:16 +0100

    added Popover component

--- src/components/store-switcher.tsx
+++ src/components/store-switcher.tsx
@@ -1,7 +1,13 @@
 "use client"
 
-import { PopoverTrigger } from "@/components/ui/popover"
+import { Popover, PopoverTrigger } from "@/components/ui/popover"
+import { useStoreModal } from "@/hooks/use-store-modal"
 import { Store } from "@prisma/client"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { ChevronsUpDown, Store as StoreIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -13,5 +19,41 @@ export default function StoreSwitcher({
   className,
   items = [],
 }: StoreSwitcherProps) {
-  return <div>Store switcher</div>
+  const storeModal = useStoreModal()
+  const params = useParams()
+  const router = useRouter()
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }))
+
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  )
+
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setIsOpen(false)
+    router.push(`/${store.value}`)
+  }
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={isOpen}
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          Current Store
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  )
 }
